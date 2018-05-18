c0745871aef1dfc5aad87230f8c23061a121f1ac
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-18 22:19:39 +0800

    Do not re-render if state does not change

--- src/react-redux.js
+++ src/react-redux.js
@@ -1,30 +1,41 @@
 import React from 'react';
 
-let globalStore;
+let _globalStore;
 
 export const Provider = ({ store, children }) => {
-  globalStore = store;
+  _globalStore = store;
   return <div>{children}</div>;
 };
 
 export function connect(mapStateToProps, mapDispatchToProps) {
   return function (Component) {
-    class HOC extends React.Component {
-      state = { props: mapStateToProps(globalStore.getState()) };
-
+    class Connected extends React.Component {
       constructor() {
         super();
-        globalStore.subscribe(() => {
-          const props = mapStateToProps(globalStore.getState());
-          this.setState({ props });
+
+        this.state = {};
+        const props = mapStateToProps(_globalStore.getState());
+        const newKeys = Object.keys(props);
+        newKeys.forEach((key) => {
+          this.state[key] = props[key];
+        });
+
+        _globalStore.subscribe(() => {
+          const props = mapStateToProps(_globalStore.getState());
+          const newKeys = Object.keys(props);
+          newKeys.forEach((key) => {
+            if (this.state[key] !== props[key]) { // shallow comparison
+              this.setState({ [key]: props[key] });
+            }
+          })
         });
       }
 
       render() {
-        return <Component {...Component.props} {...this.state.props} />;
+        return <Component {...Component.props} {...this.state} />;
       }
     }
 
-    return HOC;
+    return Connected;
   };
 }
