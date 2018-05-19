ccb74db98184a9adbffa0b30e4c17ba45e57fbe0
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-19 11:21:23 +0800

    Advanced todo list: Convert TaskList to class component

--- examples/TodoListAdvanced/components/TaskList.js
+++ examples/TodoListAdvanced/components/TaskList.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Task from './Task';
 
-const TaskList = (props) => {
-  const { tasks, editTask } = props;
-  const onTaskClick = (idx, done) => {
+class TaskList extends Component {
+  onTaskClick = (idx, done) => {
+    const { editTask } = this.props;
     if (done) {
       editTask(idx, { done: false });
     } else {
@@ -11,23 +11,41 @@ const TaskList = (props) => {
     }
   };
 
-  return (
-    <div>
-      <h2>Task List</h2>
-      <p>Completed: {tasks.filter(t => t.done).length}</p>
-      <p>To be done: {tasks.filter(t => !t.done).length}</p>
-      {tasks.map((t, idx) => {
-        return (
-          <Task
-            key={t.title}
-            title={t.title}
-            done={t.done}
-            onClick={() => onTaskClick(idx, t.done)}
-          />
-        );
-      })}
-    </div>
-  );
-};
+  renderTaskList = () => {
+    const { tasks } = this.props;
+    return (
+      <div>
+        {tasks.map((t, idx) => {
+          return (
+            <Task
+              key={t.title}
+              title={t.title}
+              done={t.done}
+              onClick={() => this.onTaskClick(idx, t.done)}
+            />
+          );
+        })}
+      </div>
+    );
+  };
+
+  renderTaskForm = () => {
+
+  };
+
+  render() {
+    const { tasks } = this.props;
+
+    return (
+      <div>
+        <h2>Task List</h2>
+        <p>Completed: {tasks.filter(t => t.done).length}</p>
+        <p>To be done: {tasks.filter(t => !t.done).length}</p>
+        {this.renderTaskList()}
+        {this.renderTaskForm()}
+      </div>
+    );
+  }
+}
 
 export default TaskList;
