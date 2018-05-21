269f8300e6c560b9068c77f07657899897e6eeb3
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-21 15:21:41 +0800

    react-redux: Add more tests

--- tests/react-redux.test.js
+++ tests/react-redux.test.js
@@ -12,87 +12,169 @@ import { Provider, connect } from '../src/react-redux';
 import { countReducer } from './fixture/countReducer';
 import { taskReducer } from './fixture/taskReducer';
 
-it('re-renders when state has been updated', () => {
-  const mockFn = jest.fn();
+describe('re-renders correctly', () => {
+  it('re-renders when state has been updated', () => {
+    const mockFn = jest.fn();
 
-  class Counter extends Component {
-    render() {
-      mockFn(this.props.count);
-      return (
-        <p>{this.props.count}</p>
-      );
+    class Counter extends Component {
+      render() {
+        mockFn(this.props.count);
+        return (
+          <p>{this.props.count}</p>
+        );
+      }
     }
-  }
 
-  const mapStateToProps = (state) => ({
-    count: state,
+    const mapStateToProps = (state) => ({
+      count: state,
+    });
+
+    const ConnectedCounter = connect(mapStateToProps)(Counter);
+
+    const store = createStore(countReducer);
+    const app = (
+      <Provider store={store}>
+        <ConnectedCounter />
+      </Provider>
+    );
+
+    const mounted = mount(app);
+    expect(mockFn).toHaveBeenLastCalledWith(0);
+
+    store.dispatch({ type: 'INCREMENT' });
+    expect(mockFn).toHaveBeenLastCalledWith(1);
+
+    store.dispatch({ type: 'INCREMENT' });
+    expect(mockFn).toHaveBeenLastCalledWith(2);
+
+    store.dispatch({ type: 'INCREMENT' });
+    expect(mockFn).toHaveBeenLastCalledWith(3);
+
+    store.dispatch({ type: 'DECREMENT' });
+    expect(mockFn).toHaveBeenLastCalledWith(2);
+
+    expect(mockFn).toHaveBeenCalledTimes(5);
+    mounted.unmount();
   });
 
-  const ConnectedCounter = connect(mapStateToProps)(Counter);
+  it('re-renders only when related state has been updated', () => {
+    const mockFn = jest.fn();
 
-  const store = createStore(countReducer);
-  const app = (
-    <Provider store={store}>
-      <ConnectedCounter />
-    </Provider>
-  );
+    class Task extends Component {
+      render() {
+        mockFn(this.props.title);
+        return (
+          <p>{this.props.title}</p>
+        );
+      }
+    }
 
-  mount(app);
-  expect(mockFn).toHaveBeenLastCalledWith(0);
+    const mapStateToProps = (state) => ({
+      title: state.title,
+    });
 
-  store.dispatch({ type: 'INCREMENT' });
-  expect(mockFn).toHaveBeenLastCalledWith(1);
+    const ConnectedTask = connect(mapStateToProps)(Task);
 
-  store.dispatch({ type: 'INCREMENT' });
-  expect(mockFn).toHaveBeenLastCalledWith(2);
+    const store = createStore(taskReducer);
+    const app = (
+      <Provider store={store}>
+        <ConnectedTask />
+      </Provider>
+    );
 
-  store.dispatch({ type: 'INCREMENT' });
-  expect(mockFn).toHaveBeenLastCalledWith(3);
+    const mounted = mount(app);
+    expect(mockFn).toHaveBeenLastCalledWith('Title');
 
-  store.dispatch({ type: 'DECREMENT' });
-  expect(mockFn).toHaveBeenLastCalledWith(2);
+    store.dispatch({ type: 'CHANGE_TITLE', title: 'new title' });
+    expect(mockFn).toHaveBeenLastCalledWith('new title');
+    expect(mockFn).toHaveBeenCalledTimes(2);
 
-  expect(mockFn).toHaveBeenCalledTimes(5);
+    store.dispatch({ type: 'CHANGE_DESCRIPTION', description: 'new description' });
+    expect(mockFn).toHaveBeenCalledTimes(2); // no re-rendering
+    mounted.unmount();
+  });
 });
 
-it('re-renders only when related state has been updated', () => {
-  const mockFn = jest.fn();
+describe('handles props, state, dispatch correctly', () => {
+  let mockStore;
+  let spy;
+  beforeEach(() => {
+    const identityReducer = (state, action) => action.newState;
+    mockStore = createStore(identityReducer);
+    spy = jest.fn();
 
-  class Task extends Component {
-    render() {
-      mockFn(this.props.title);
-      return (
-        <p>{this.props.title}</p>
-      );
-    }
-  }
-
-  const mapStateToProps = (state) => ({
-    title: state.title,
+    mount(<Provider store={mockStore} />);
   });
 
-  const ConnectedTask = connect(mapStateToProps)(Task);
+  it('passes props from connected component to original component', () => {
+    class Comp extends Component {
+      componentWillMount() {
+        spy(this.props.propPassedToConnectedComp);
+      }
 
-  const store = createStore(taskReducer);
-  const app = (
-    <Provider store={store}>
-      <ConnectedTask />
-    </Provider>
-  );
+      render() {
+        return <div />;
+      }
+    }
 
-  mount(app);
-  expect(mockFn).toHaveBeenLastCalledWith('Title');
+    const ConnectedComp = connect()(Comp);
+    const mounted = mount(<ConnectedComp propPassedToConnectedComp={'foo'} />);
 
-  store.dispatch({ type: 'CHANGE_TITLE', title: 'new title' });
-  expect(mockFn).toHaveBeenLastCalledWith('new title');
-  expect(mockFn).toHaveBeenCalledTimes(2);
+    expect(spy).toHaveBeenLastCalledWith('foo');
+    mounted.unmount();
+  });
 
-  store.dispatch({ type: 'CHANGE_DESCRIPTION', description: 'new description' });
-  expect(mockFn).toHaveBeenCalledTimes(2); // no re-rendering
+  it('maps state to props correctly', () => {
+    class Comp extends Component {
+      componentWillMount() {
+        spy(this.props.propMappedFromState);
+      }
+
+      render() {
+        return <div />;
+      }
+    }
+
+    const ConnectedComp = connect(state => ({ propMappedFromState: state }))(Comp);
+    mockStore.dispatch({ type: '', newState: 'foo' });
+    const mounted = mount(<ConnectedComp />);
+
+    expect(spy).toHaveBeenLastCalledWith('foo');
+
+    mounted.unmount();
+  });
+
+  describe('it maps dispatch to props', () => {
+    let Comp;
+    beforeEach(() => {
+      Comp = class extends Component {
+        componentWillMount() {
+          this.props.doIt();
+        }
+
+        render() {
+          return <div />;
+        }
+      };
+    });
+    it('maps dispatch to props - mapDispatchToProps as an object', () => {
+      const mockFn = jest.fn(() => ({ type: '' }));
+      const ConnectedComp = connect(undefined, { doIt: mockFn })(Comp);
+      const mounted = mount(<ConnectedComp />);
+      expect(mockFn).toHaveBeenCalledTimes(1);
+
+      mounted.unmount();
+    });
+
+    it('maps dispatch to props - mapDispatchToProps as a function', () => {
+      const mockFn = jest.fn(() => ({ type: '' }));
+      const ConnectedComp = connect(undefined, (dispatch) => ({ doIt: (...args) => dispatch(mockFn(...args)) }))(Comp);
+      const mounted = mount(<ConnectedComp />);
+      expect(mockFn).toHaveBeenCalledTimes(1);
+
+      mounted.unmount();
+    });
+
+  });
+
 });
-
-it('maps state to props correctly');
-
-it('maps dispatch to props correctly (mapDispatchToProps as an object)');
-
-it('maps dispatch to props correctly (mapDispatchToProps as a function)');
